cfb4ad2374986e98fb0d961c0cd521a40d962ca4
sirserg <sirserg05@gmail.com>
2024-11-09 00:29:02 +0200

    feat: add frontend env

--- frontend/src/common/enums/api/api-path.enum.ts
+++ frontend/src/common/enums/api/api-path.enum.ts
@@ -1,5 +1,5 @@
 const ApiPath = {
-  API_URL: import.meta.env.API_PATH || 'http://localhost:3000',
+  API_URL: import.meta.env.VITE_API_PATH || 'http://localhost:3000',
 
   // Auth routes
   AUTH: '/auth',
